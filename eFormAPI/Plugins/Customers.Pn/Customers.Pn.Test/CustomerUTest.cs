b3510b35d6d1295727f6a8f3cae42ec1a563f2f0
Mathias Husum Nielsen <soipipable@gmail.com>
2018-11-08 15:31:48 +0100

    more test work, saving current state of work.

--- eFormAPI/Plugins/Customers.Pn/Customers.Pn.Test/CustomerUTest.cs
+++ eFormAPI/Plugins/Customers.Pn/Customers.Pn.Test/CustomerUTest.cs
@@ -46,5 +46,114 @@ namespace Customers.Pn.Test
             Assert.AreEqual(customerFullModel.Phone , customer.Phone);
             Assert.AreEqual(customerFullModel.ZipCode , customer.ZipCode);
         }
+        //needs version.
+        [Test]
+        public void CustomerFullModel_Update_DoesUpdate()
+        {
+            //// Arrange
+            //Customer customer = new Customer();
+            //customer.CityName = Guid.NewGuid().ToString();
+            //customer.CompanyAddress = Guid.NewGuid().ToString();
+            //customer.CompanyName = Guid.NewGuid().ToString();
+            //customer.ContactPerson = Guid.NewGuid().ToString();
+            //customer.CreatedBy = Guid.NewGuid().ToString();
+            //customer.CustomerNo = Guid.NewGuid().ToString();
+            //customer.Description = Guid.NewGuid().ToString();
+            //customer.Email = Guid.NewGuid().ToString();
+            //customer.Phone = Guid.NewGuid().ToString();
+            //customer.ZipCode = Guid.NewGuid().ToString();
+
+            //DbContext.Customers.Add(customer);
+            //DbContext.SaveChanges();
+
+            //// Act
+            //CustomerFullModel customerFullModel = new CustomerFullModel();
+            //customerFullModel.CityName = customer.CityName;
+            //customerFullModel.CompanyAddress = customer.CompanyAddress;
+            //customerFullModel.CompanyName = customer.CompanyName;
+            //customerFullModel.ContactPerson = customer.ContactPerson;
+            //customerFullModel.CreatedBy = customer.CreatedBy;
+            //customerFullModel.CustomerNo = customer.CustomerNo;
+            //customerFullModel.Description = customer.Description;
+            //customerFullModel.Email = customer.Email;
+            //customerFullModel.Phone = customer.Phone;
+            //customerFullModel.ZipCode = customer.ZipCode;
+
+            //customerFullModel.Update(DbContext);
+
+            //Customer dbCustomer = DbContext.Customers.AsNoTracking().First();
+            //List<Customer> customerList = DbContext.Customers.AsNoTracking().ToList();
+
+            //// Assert
+            //Assert.NotNull(dbCustomer);
+
+            //Assert.AreEqual(1, customerList.Count());
+
+            //Assert.AreEqual(customer.CityName, dbCustomer.CityName);
+            //Assert.AreEqual(customer.CompanyAddress, dbCustomer.CompanyAddress);
+            //Assert.AreEqual(customer.CompanyName, dbCustomer.CompanyName);
+            //Assert.AreEqual(customer.ContactPerson, dbCustomer.ContactPerson);
+            //Assert.AreEqual(customer.CreatedBy, dbCustomer.CreatedBy);
+            //Assert.AreEqual(customer.CustomerNo, dbCustomer.CustomerNo);
+            //Assert.AreEqual(customer.Description, dbCustomer.Description);
+            //Assert.AreEqual(customer.Email, dbCustomer.Email);
+            //Assert.AreEqual(customer.Phone, dbCustomer.Phone);
+            //Assert.AreEqual(customer.ZipCode, dbCustomer.ZipCode);
+        }
+        //needs versions.
+        [Test]
+        public void CustomerFullModel_Delete_DoesDelete()
+        {
+            //// Arrange
+            //Customer customer = new Customer();
+            //customer.CityName = Guid.NewGuid().ToString();
+            //customer.CompanyAddress = Guid.NewGuid().ToString();
+            //customer.CompanyName = Guid.NewGuid().ToString();
+            //customer.ContactPerson = Guid.NewGuid().ToString();
+            //customer.CreatedBy = Guid.NewGuid().ToString();
+            //customer.CustomerNo = Guid.NewGuid().ToString();
+            //customer.Description = Guid.NewGuid().ToString();
+            //customer.Email = Guid.NewGuid().ToString();
+            //customer.Phone = Guid.NewGuid().ToString();
+            //customer.ZipCode = Guid.NewGuid().ToString();
+
+            //DbContext.Customers.Add(customer);
+            //DbContext.SaveChanges();
+
+            //// Act
+            //CustomerFullModel customerFullModel = new CustomerFullModel();
+            //customerFullModel.CityName = customer.CityName;
+            //customerFullModel.CompanyAddress = customer.CompanyAddress;
+            //customerFullModel.CompanyName = customer.CompanyName;
+            //customerFullModel.ContactPerson = customer.ContactPerson;
+            //customerFullModel.CreatedBy = customer.CreatedBy;
+            //customerFullModel.CustomerNo = customer.CustomerNo;
+            //customerFullModel.Description = customer.Description;
+            //customerFullModel.Email = customer.Email;
+            //customerFullModel.Phone = customer.Phone;
+            //customerFullModel.ZipCode = customer.ZipCode;
+
+            //customerFullModel.Update(DbContext);
+
+            //Customer dbCustomer = DbContext.Customers.AsNoTracking().First();
+            //List<Customer> customerList = DbContext.Customers.AsNoTracking().ToList();
+
+            //// Assert
+            //Assert.NotNull(dbCustomer);
+
+            //Assert.AreEqual(1, customerList.Count());
+
+            //Assert.AreEqual(customer.CityName, dbCustomer.CityName);
+            //Assert.AreEqual(customer.CompanyAddress, dbCustomer.CompanyAddress);
+            //Assert.AreEqual(customer.CompanyName, dbCustomer.CompanyName);
+            //Assert.AreEqual(customer.ContactPerson, dbCustomer.ContactPerson);
+            //Assert.AreEqual(customer.CreatedBy, dbCustomer.CreatedBy);
+            //Assert.AreEqual(customer.CustomerNo, dbCustomer.CustomerNo);
+            //Assert.AreEqual(customer.Description, dbCustomer.Description);
+            //Assert.AreEqual(customer.Email, dbCustomer.Email);
+            //Assert.AreEqual(customer.Phone, dbCustomer.Phone);
+            //Assert.AreEqual(customer.ZipCode, dbCustomer.ZipCode);
+            //Assert.AreEqual(customer.workflow_state, eFormShared.Constants.WorkflowStates.Removed);
+        }
     }
 }
---
+++ eFormAPI/Plugins/Customers.Pn/Customers.Pn.Test/FieldUTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Customers.Pn.Infrastructure.Data.Entities;
+using Customers.Pn.Infrastructure.Models.Fields;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Customers.Pn.Test
+{
+    [TestFixture]
+    public class FieldUTest : DbTestFixture
+    {
+        [Test]
+        public void FieldModel_Save_DoesSave()
+        {
+            // Arrange
+            FieldModel fieldModel = new FieldModel();
+            fieldModel.Name = Guid.NewGuid().ToString();
+            fieldModel.Value = Guid.NewGuid().ToString();
+
+            // Act
+            fieldModel.Save(DbContext);
+
+            Field dbField = DbContext.Fields.AsNoTracking().First();
+            List<Field> fieldList = DbContext.Fields.AsNoTracking().ToList();
+            // Assert
+
+            Assert.NotNull(dbField);
+
+            Assert.AreEqual(1, fieldList.Count());
+
+            Assert.AreEqual(fieldModel.Name, dbField.Name);
+            
+        }
+        [Test]
+        public void FieldUpdateModel_Udate_DoesUpdate()
+        {
+
+        }
+    }
+}
