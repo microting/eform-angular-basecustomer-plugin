b72c48c5ca36e0fe87a97d1738a427db4241d187
René Schultz Madsen <rm@microting.dk>
2019-01-09 11:37:51 +0100

    Adding initial tests.

---
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
@@ -0,0 +1,9 @@
+import loginPage from '../../Page objects/Login.page';
+
+import {expect} from 'chai';
+
+describe('Application settings page - site header section', function () {
+    before(function () {
+        loginPage.open('/auth');
+    });
+});
