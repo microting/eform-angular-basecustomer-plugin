f8ad15be6fdc7b9d35a14145c360ed71fc3a3ab5
René Schultz Madsen <rm@microting.dk>
2019-01-10 08:12:06 +0100

    Adding the first test for wether the plugin is installed or not.

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
@@ -2,6 +2,7 @@ import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
 
 import {expect} from 'chai';
+import pluginsPage from './application-settings.plugins.page';
 
 describe('Application settings page - site header section', function () {
     before(function () {
@@ -10,8 +11,14 @@ describe('Application settings page - site header section', function () {
     it('should go to plugin settings page', function () {
        loginPage.login();
        myEformsPage.Navbar.advancedDropdown();
-       myEformsPage.Navbar.clickonSubMenuItem('PluginSettings');
+       myEformsPage.Navbar.clickonSubMenuItem('Plugin Settings');
        browser.pause(8000);
+
+      const plugin = pluginsPage.getFirstPluginRowObj();
+      expect(plugin.id).equal(1);
+      expect(plugin.name).equal('Microting Customers plugin');
+      expect(plugin.version).equal('1.0.0.0');
+      expect(plugin.status).equal('Deaktiveret');
        // expect()
 
     });
