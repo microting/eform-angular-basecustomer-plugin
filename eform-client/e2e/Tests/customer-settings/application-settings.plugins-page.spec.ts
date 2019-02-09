ad77e1fa03664f7df8e6363f3e41768cd4308620
René Schultz Madsen <rm@microting.com>
2019-02-09 16:16:14 +0100

    Adding a pause and refresh after activating the plugin.

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
@@ -32,6 +32,8 @@ describe('Application settings page - site header section', function () {
       pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
       browser.pause(8000);
       pluginPage.saveBtn.click();
+      browser.pause(20000);
+      browser.refresh()
         // click on plugin settings
         // enter connectionstring for customers plugin
         // select activate
