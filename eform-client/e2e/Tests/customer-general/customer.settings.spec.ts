5b438e8806e25abb44fc6fe8dbad1a42849afe7f
Yehor <gid733@ya.ru>
2019-02-19 12:20:51 +0200

    Fix sogbar lister

--- eform-client/e2e/Tests/customer-general/customer.settings.spec.ts
+++ eform-client/e2e/Tests/customer-general/customer.settings.spec.ts
@@ -14,7 +14,7 @@ describe('Customers plugin settings page', function () {
   it('should create searchable list', function () {
     myEformsPage.Navbar.advancedDropdown();
     browser.pause(1000);
-    myEformsPage.Navbar.clickonSubMenuItem('Søgbar lister');
+    myEformsPage.Navbar.clickonSubMenuItem('Søgbar Lister');
     browser.pause(4000);
     // user see Searchable lists page and new list button (#createEntitySearchBtn) within it
     const newSearchListBtn = $('#createEntitySearchBtn');
