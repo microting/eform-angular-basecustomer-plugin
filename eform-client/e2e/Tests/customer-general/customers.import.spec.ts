3448a0f7c136cf0327986ce93e0fb6dd4b1396b0
Konstantin <thousandkats@gmail.com>
2019-02-19 18:18:31 +0700

    Fixed path in import test, assets folder added

--- eform-client/e2e/Tests/customer-general/customers.import.spec.ts
+++ eform-client/e2e/Tests/customer-general/customers.import.spec.ts
@@ -11,22 +11,26 @@ describe('Customers plugin import page', function () {
     customersPage.goToCustomersPage();
   });
   it('should import customers list', function () {
+    const localPath = process.cwd();
     const customersBefore = customersImportPage.numberOfCustomers;
     const importButton = customersPage.importCustomerBtn();
     importButton.click();
     browser.pause(8000);
-    browser.chooseFile('#files', 'C:\\Import-test.csv');
+    browser.chooseFile('#files', localPath + '\\e2e\\Assets\\Import-test.csv');
+    browser.pause(8000);
     // press 'continue import' button
     customersImportPage.continueImport();
     const customersAfter = customersImportPage.numberOfCustomers;
     expect(customersAfter, 'Number of customers is not bigger than before').greaterThan(customersBefore)
   });
   it('should not import customers', function () {
+    const localPath = process.cwd();
     const customersBefore = customersImportPage.numberOfCustomers;
     const importButton = customersPage.importCustomerBtn();
     importButton.click();
     browser.pause(8000);
-    browser.chooseFile('#files', 'C:\\Import-test.csv');
+    browser.chooseFile('#files', localPath + '\\e2e\\Assets\\Import-test.csv');
+    browser.pause(8000);
     customersImportPage.cancelImport();
 
     const customersAfter = customersImportPage.numberOfCustomers;
