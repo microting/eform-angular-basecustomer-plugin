529203284586e524159cd684bc327d41b2d05699
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-10-29 13:23:16 +0100

    Adding new changes to support the new plugin permission loading c# part.

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
@@ -28,23 +28,16 @@ describe('Application settings page - site header section', function () {
         pluginPage.pluginSettingsBtn.click();
         browser.waitForVisible('#PluginDropDown', 40000);
         pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
-        // browser.pause(8000);
         pluginPage.saveBtn.click();
         browser.pause(50000); // We need to wait 50 seconds for the plugin to create db etc.
         browser.refresh();
 
-        // Start - This block is here because of the new plugin permission loading, requires a re-login.
-        browser.waitForVisible('#plugin-id', 40000);
-        browser.pause(10000);
-        myEformsPage.Navbar.logout();
         loginPage.login();
         myEformsPage.Navbar.advancedDropdown();
         myEformsPage.Navbar.clickonSubMenuItem('Plugins');
         browser.waitForExist('#plugin-name', 50000);
         browser.pause(10000);
-        // End - This block is here because of the new plugin permission loading, requires a re-login.
 
-        browser.waitForVisible('#plugin-id', 40000);
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Customers plugin');
