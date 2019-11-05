f713d764f1fd613b59e21104bb0fd26ec63aee16
René Schultz Madsen <rm@microting.dk>
2019-11-05 10:36:41 +0100

    Adding a trailing , to the plugin route.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -3,5 +3,5 @@ sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i path: "customers-pn",' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/customers-pn/customers-pn.module#CustomersPnModule"' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i },' src/app/plugins/plugins.routing.ts -i
 
