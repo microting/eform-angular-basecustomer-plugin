3770f1c0a0d30ec60025bb95ea1ef9f5074a7e5e
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-11-28 09:49:31 +0100

    Is now looking for the correct Btn.

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins.page.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins.page.ts
@@ -40,7 +40,7 @@ class PluginRowObject {
             this.name = $$('#plugin-name')[rowNum - 1].getText();
             this.version = $$('#plugin-version')[rowNum - 1].getText();
             this.status = $$('#plugin-status')[rowNum - 1].getText();
-            this.settingsBtn = $$('#plugin-settings-btn')[rowNum - 1].getText();
+            this.settingsBtn = $$('#plugin-settings-link')[rowNum - 1].getText();
         }
     }
 
