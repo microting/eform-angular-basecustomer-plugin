6f4155ba860ed4e9ef861cdec9b5c008b46fa7a9
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-11-28 10:28:32 +0100

    Removing unwanted .getText()

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins.page.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins.page.ts
@@ -39,7 +39,7 @@ class PluginRowObject {
             this.id = +$$('#plugin-id')[rowNum - 1].getText();
             this.name = $$('#plugin-name')[rowNum - 1].getText();
             this.version = $$('#plugin-version')[rowNum - 1].getText();
-            this.settingsBtn = $$('#plugin-settings-link')[rowNum - 1].getText();
+            this.settingsBtn = $$('#plugin-settings-link')[rowNum - 1];
         }
     }
 
