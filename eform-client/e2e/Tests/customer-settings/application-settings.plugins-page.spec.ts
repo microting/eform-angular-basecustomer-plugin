00fb071af4c14f98094c7f2149fc1813d761efbe
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-12-05 10:16:07 +0100

    Looking for correct spelling of Plugin.

--- eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/customer-settings/application-settings.plugins-page.spec.ts
@@ -18,7 +18,7 @@ describe('Application settings page - site header section', function () {
 
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
-        expect(plugin.name).equal('Microting Customers plugin');
+        expect(plugin.name).equal('Microting Customers Plugin');
         expect(plugin.version).equal('1.0.0.0');
         // expect()
 
@@ -38,7 +38,7 @@ describe('Application settings page - site header section', function () {
     
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
-        expect(plugin.name).equal('Microting Customers plugin');
+        expect(plugin.name).equal('Microting Customers Plugin');
         expect(plugin.version).equal('1.0.0.0');
         expect(browser.element(`//*[contains(text(), 'Kunder')]`).isExisting()).equal(true);
 
