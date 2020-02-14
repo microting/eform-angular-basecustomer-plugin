abb2cfc7f3b1881a9e4a8809278ee3b4d1691a8c
René Schultz Madsen <rm@microting.com>
2020-02-14 08:03:33 +0100

    Updating the dev files.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,14 +2,26 @@
 
 cd ~
 
-if [ -d "Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/src/app/plugins/modules/customers-pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/src/app/plugins/modules/customers-pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/src/app/plugins/modules/customers-pn
 
 cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/customers-pn Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/src/app/plugins/modules/customers-pn
 
-if [ -d "Documents/workspace/microting/eform-angular-basecustomer-plugin/eFormAPI/Plugins/Customers.Pn" ]; then
-	rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eFormAPI/Plugins/Customers.Pn
-fi
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eFormAPI/Plugins/Customers.Pn
 
 cp -av Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Customers.Pn Documents/workspace/microting/eform-angular-basecustomer-plugin/eFormAPI/Plugins/Customers.Pn
+
+# Test files rm
+
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/customer-settings
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/customer-general 
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Assets/
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Page\ objects/Customers
+rm -fR Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+
+# Test files cp
+
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-settings Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/customer-settings
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/customer-general Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/customer-general 
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Assets Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Assets/
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/Customers Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Page\ objects/Customers
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/wdio-headless-plugin-step2.conf.js
