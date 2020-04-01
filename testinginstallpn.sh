15d1d4182a49989d0b552aa37906ae2feea519d4
René Schultz Madsen <rm@microting.dk>
2020-04-01 07:32:37 +0200

    Adding the missing , to seperate plugins.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perl -pi -e '$_.="  }\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/customers-pn/customers-pn.module#CustomersPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  path: '\''customers-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
