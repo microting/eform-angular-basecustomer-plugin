a4f908d59cb6db92e881db8054ce74de8ab112ee
René Schultz Madsen <rm@microting.dk>
2020-04-17 13:12:09 +0200

    Increasing the timeout.

--- eform-client/wdio-headless-plugin-step2.conf.js
+++ eform-client/wdio-headless-plugin-step2.conf.js
@@ -159,7 +159,7 @@ exports.config = {
     ui: 'bdd',
     require: 'ts-node/register',
     compilers: ['tsconfig-paths/register'],
-    timeout: 60000
+    timeout: 6000000
   },
   //
   // =====
