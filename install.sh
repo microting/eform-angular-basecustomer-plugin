965e6ad023c3aa663ee777f169dd07b65aa80ac8
René Schultz Madsen <rm@microting.com>
2020-04-23 11:56:13 +0200

    Updating the install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,5 @@ cd /var/www/microting/eform-angular-frontend/eform-client
 su ubuntu -c \
 "/var/www/microting/eform-angular-basecustomer-plugin/testinginstallpn.sh"
 su ubuntu -c \
-"npm run build"
+"export NODE_OPTIONS=--max_old_space_size=8192 && time GENERATE_SOURCEMAP=false npm run build"
 echo "Recompiling angular done"
-
-
