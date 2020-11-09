09392d78e37fbe0c9fa90701f3359bc1a2cdfc82
René Schultz Madsen <rm@microting.dk>
2020-11-09 10:11:37 +0000

    Moving another navigation.

--- eform-client/e2e/Tests/customer-general/customer.copy.spec.ts
+++ eform-client/e2e/Tests/customer-general/customer.copy.spec.ts
@@ -9,7 +9,6 @@ describe('Customers plugin page', function () {
   before(function () {
     loginPage.open('/');
     loginPage.login();
-    customersPage.goToCustomersPage();
   });
   // it('should add new customer with all empty fields', function () {
   //   $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
@@ -22,6 +21,7 @@ describe('Customers plugin page', function () {
   //   expect(rowCountAfterCreation, 'Number of rows hasn\'t changed after creating new customer').equal(rowCountBeforeCreation + 1);
   // });
   it('should add new customer with all fields', function () {
+    customersPage.goToCustomersPage();
     customersPage.newCustomerBtn.click();
     $('#createCustomerNo').waitForDisplayed({timeout: 20000});
     $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
