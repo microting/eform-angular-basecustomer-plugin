3e5022255ea41c620a4fc97d0f6df7ac4e0d8e52
René Schultz Madsen <rm@microting.dk>
2020-11-09 12:04:13 +0000

    Making it headless.

--- eform-client/wdio-headless-plugin-step2.conf.js
+++ eform-client/wdio-headless-plugin-step2.conf.js
@@ -61,7 +61,7 @@ exports.config = {
     browserName: 'chrome',
     'goog:chromeOptions': {
       args: [
-        //'headless',
+        'headless',
         'window-size=1920,1080',
         'disable-gpu'],
     },
