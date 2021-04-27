32e7018bcfbad50084533f6646c03ac975a80994
Alex <sahok6264@gmail.com>
2021-04-27 18:11:02 +0300

    Change tests

--- eform-client/e2e/Page objects/Customers/Customers.page.ts
+++ eform-client/e2e/Page objects/Customers/Customers.page.ts
@@ -1,5 +1,5 @@
 import Page from '../Page';
-import {PageWithNavbarPage} from '../PageWithNavbar.page';
+import { PageWithNavbarPage } from '../PageWithNavbar.page';
 
 export class CustomersPage extends PageWithNavbarPage {
   constructor() {
@@ -9,31 +9,42 @@ export class CustomersPage extends PageWithNavbarPage {
   public rowNum(): number {
     return browser.$$('#mainTableBody > tr').length;
   }
-  public clickIdSort() {
-    $('#IdTableHeader').waitForDisplayed({timeout: 20000});
-    $('#IdTableHeader').waitForClickable({timeout: 20000});
-    browser.$('#IdTableHeader').click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
-  }
-  public clickContactSort() {
-    $('#ContactPersonTableHeader').waitForDisplayed({timeout: 20000});
-    $('#ContactPersonTableHeader').waitForClickable({timeout: 20000});
-    browser.$('#ContactPersonTableHeader').click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
+
+  public get IdTableHeader() {
+    const ele = $('#IdTableHeader');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
   }
 
-  public clickCompanySort() {
-    $('#CompanyNameTableHeader').waitForDisplayed({timeout: 20000});
-    $('#CompanyNameTableHeader').waitForClickable({timeout: 20000});
-    browser.$('#CompanyNameTableHeader').click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
+  public get contactPersonTableHeader() {
+    const ele = $('#ContactPersonTableHeader');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
+  }
+
+  public get companyNameTableHeader() {
+    const ele = $('#CompanyNameTableHeader');
+    ele.waitForDisplayed({ timeout: 20000 });
+    ele.waitForClickable({ timeout: 20000 });
+    return ele;
   }
 
   public getCustomerValue(selector: any, row: number) {
     if (selector.includes('Id')) {
-      return  parseInt( $('#mainTableBody').$(`tr:nth-child(${row})`).$('#' + selector).getText(), 10);
+      return parseInt(
+        $('#mainTableBody')
+          .$(`tr:nth-child(${row})`)
+          .$('#' + selector)
+          .getText(),
+        10
+      );
     } else {
-      return $('#mainTableBody').$(`tr:nth-child(${row})`).$('#' + selector).getText();
+      return $('#mainTableBody')
+        .$(`tr:nth-child(${row})`)
+        .$('#' + selector)
+        .getText();
     }
   }
 
@@ -42,62 +53,62 @@ export class CustomersPage extends PageWithNavbarPage {
   }
 
   public get newCustomerBtn() {
-    $('#newCustomerBtn').waitForDisplayed({timeout: 20000});
-    $('#newCustomerBtn').waitForClickable({timeout: 20000});
+    $('#newCustomerBtn').waitForDisplayed({ timeout: 20000 });
+    $('#newCustomerBtn').waitForClickable({ timeout: 20000 });
     return $('#newCustomerBtn');
   }
 
   public get customersSettingsBtn() {
-    $('#firstName').waitForDisplayed({timeout: 20000});
-    $('#firstName').waitForClickable({timeout: 20000});
+    $('#firstName').waitForDisplayed({ timeout: 20000 });
+    $('#firstName').waitForClickable({ timeout: 20000 });
     return $('#firstName');
   }
 
   public get importCustomersSettingsBtn() {
-    $('#lastName').waitForDisplayed({timeout: 20000});
-    $('#lastName').waitForClickable({timeout: 20000});
+    $('#lastName').waitForDisplayed({ timeout: 20000 });
+    $('#lastName').waitForClickable({ timeout: 20000 });
     return $('#lastName');
   }
 
   // same purpose as previous method?
-  public  importCustomerBtn() {
-    $('#importCustomer').waitForDisplayed({timeout: 20000});
-    $('#importCustomer').waitForClickable({timeout: 20000});
+  public importCustomerBtn() {
+    $('#importCustomer').waitForDisplayed({ timeout: 20000 });
+    $('#importCustomer').waitForClickable({ timeout: 20000 });
     return $('#importCustomer');
   }
 
-  public  goToImportBtn() {
+  public goToImportBtn() {
     this.importCustomerBtn().click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
+    $('#spinner-animation').waitForDisplayed({ timeout: 20000, reverse: true });
   }
 
   public get saveImportCustomersBtn() {
-    $('#saveCreateBtn').waitForDisplayed({timeout: 20000});
-    $('#saveCreateBtn').waitForClickable({timeout: 20000});
+    $('#saveCreateBtn').waitForDisplayed({ timeout: 20000 });
+    $('#saveCreateBtn').waitForClickable({ timeout: 20000 });
     return $('#saveCreateBtn');
   }
 
   public get cancelImportCustomersBtn() {
-    $('#saveCreateBtn').waitForDisplayed({timeout: 20000});
-    $('#saveCreateBtn').waitForClickable({timeout: 20000});
+    $('#saveCreateBtn').waitForDisplayed({ timeout: 20000 });
+    $('#saveCreateBtn').waitForClickable({ timeout: 20000 });
     return $('#saveCreateBtn');
   }
 
   public get deleteCustomerBtn() {
-    $('#cancelCreateBtn').waitForDisplayed({timeout: 20000});
-    $('#cancelCreateBtn').waitForClickable({timeout: 20000});
+    $('#cancelCreateBtn').waitForDisplayed({ timeout: 20000 });
+    $('#cancelCreateBtn').waitForClickable({ timeout: 20000 });
     return $('#cancelCreateBtn');
   }
 
   public get editCustomerBtn() {
-    $('#editCustomerBtn').waitForDisplayed({timeout: 20000});
-    $('#editCustomerBtn').waitForClickable({timeout: 20000});
+    $('#editCustomerBtn').waitForDisplayed({ timeout: 20000 });
+    $('#editCustomerBtn').waitForClickable({ timeout: 20000 });
     return $('#editCustomerBtn');
   }
 
   public get customersButton() {
-    $('#customers-pn').waitForDisplayed({timeout: 20000});
-    $('#customers-pn').waitForClickable({timeout: 20000});
+    $('#customers-pn').waitForDisplayed({ timeout: 20000 });
+    $('#customers-pn').waitForClickable({ timeout: 20000 });
     return $('#customers-pn');
   }
 
@@ -108,17 +119,17 @@ export class CustomersPage extends PageWithNavbarPage {
   public goToCustomerSettings() {
     const elem = browser.$('button .btn .btn-danger');
     elem.click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
+    $('#spinner-animation').waitForDisplayed({ timeout: 20000, reverse: true });
   }
 
   public goToCustomersPage() {
     this.customersButton.click();
-    $('#spinner-animation').waitForDisplayed({timeout: 20000, reverse: true});
+    $('#spinner-animation').waitForDisplayed({ timeout: 20000, reverse: true });
   }
 
   public get saveDeleteBtn() {
-    $('#customerSaveDeleteBtn').waitForDisplayed({timeout: 20000});
-    $('#customerSaveDeleteBtn').waitForClickable({timeout: 20000});
+    $('#customerSaveDeleteBtn').waitForDisplayed({ timeout: 20000 });
+    $('#customerSaveDeleteBtn').waitForClickable({ timeout: 20000 });
     return $('#customerSaveDeleteBtn');
   }
 }
@@ -128,33 +139,38 @@ export default customersPage;
 
 export class CustomersRowObject {
   constructor(rowNumber) {
-    this.createdBy = $$('#CreatedBy_' + (rowNumber - 1))[0].getText();
-    this.customerNo = $$('#CustomerNo_' + (rowNumber - 1))[0].getText();
-    this.contactPerson = $$('#ContactPerson_' + (rowNumber - 1))[0].getText();
-    this.companyName = $$('#CompanyName_' + (rowNumber - 1))[0].getText();
-    this.companyAddress = $$('#CompanyAddress_' + (rowNumber - 1))[0].getText();
-    this.zipCode = $$('#ZipCode_' + (rowNumber - 1))[0].getText();
-    this.cityName = $$('#CityName_' + (rowNumber - 1))[0].getText();
-    this.email = $$('#Email_' + (rowNumber - 1))[0].getText();
-    this.phone = $$('#Phone_' + (rowNumber - 1))[0].getText();
-    this.editBtn = $$('#editCustomerBtn_' + (rowNumber - 1))[0];
-    this.copyBtn = $$('#copyCustomerBtn_' + (rowNumber - 1))[0];
-    this.deleteBtn = $$('#deleteCustomerBtn_' + (rowNumber - 1))[0];
+    this.element = $$('#mainTableBody > tr')[rowNumber - 1];
+    if (this.element) {
+      this.id = +this.element.$('#idCustomer').getText();
+      this.createdBy = this.element.$('#createdBy').getText();
+      this.customerNo = this.element.$('#customerNo').getText();
+      this.contactPerson = this.element.$('#contactPerson').getText();
+      this.companyName = this.element.$('#companyName').getText();
+      this.companyAddress = this.element.$('#companyAddress').getText();
+      this.zipCode = this.element.$('#zipCode').getText();
+      this.cityName = this.element.$('#cityName').getText();
+      this.email = this.element.$('#email').getText();
+      this.phone = this.element.$('#phone').getText();
+      this.editBtn = this.element.$('#editCustomerBtn');
+      this.copyBtn = this.element.$('#copyCustomerBtn');
+      this.deleteBtn = this.element.$('#deleteCustomerBtn');
+    }
   }
 
-  public id;
-  public version;
-  public updatedByUserId;
-  public createdBy;
-  public customerNo;
-  public contactPerson;
-  public companyName;
-  public companyAddress;
-  public zipCode;
-  public cityName;
-  public email;
-  public phone;
-  public editBtn;
-  public copyBtn;
-  public deleteBtn;
+  public element: WebdriverIO.Element;
+  public id: number;
+  public version: number;
+  public updatedByUserId: number;
+  public createdBy: string;
+  public customerNo: string;
+  public contactPerson: string;
+  public companyName: string;
+  public companyAddress: string;
+  public zipCode: string;
+  public cityName: string;
+  public email: string;
+  public phone: string;
+  public editBtn: WebdriverIO.Element;
+  public copyBtn: WebdriverIO.Element;
+  public deleteBtn: WebdriverIO.Element;
 }
